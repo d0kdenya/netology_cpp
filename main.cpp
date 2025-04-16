dec3a0ca32a6cb968f6a03bf6ff6ac118cb0a464
Dan Losev <shocker.nn@mail.ru>
2025-04-16 23:36:01 +0300

    Добавил решение задачи Счета

--- main.cpp
+++ main.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
+#include <string>
+#include <limits>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+struct Account {
+    int number;
+    std::string owner;
+    double balance;
+};
+
+void updateBalance(Account &acc, double newBalance) {
+    acc.balance = newBalance;
+}
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    system("chcp 65001 > nul");
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    Account acc;
+
+    std::cout << "Введите номер счёта: ";
+    std::cin >> acc.number;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::cout << "Введите имя владельца: ";
+    std::getline(std::cin, acc.owner);
+
+    std::cout << "Введите баланс: ";
+    std::cin >> acc.balance;
+
+    double newBal;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newBal;
+
+    updateBalance(acc, newBal);
+
+    std::cout << "Ваш счёт: "
+              << acc.owner << ", "
+              << acc.number << ", "
+              << acc.balance
+              << std::endl;
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
