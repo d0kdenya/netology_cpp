fa706bee3c99e71c3db3f41133239db09f738b19
Dan Losev <shocker.nn@mail.ru>
2025-04-17 17:01:34 +0300

    Добавил решение задачи Препроцессоры

--- main.cpp
+++ main.cpp
@@ -1,25 +1,36 @@
+#define MODE 1
+
+#ifndef MODE
+#   error "Необходимо определить MODE (0 — тренировка, 1 — боевой)"
+#endif
+
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+#if MODE == 1
+int add(int a, int b)
+{
+    return a + b;
+}
+#endif
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+int main()
+{
+    system("chcp 65001 > nul");
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки\n";
 
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме\n";
+    int x{}, y{};
+    std::cout << "Введите число 1: ";
+    std::cin  >> x;
+    std::cout << "Введите число 2: ";
+    std::cin  >> y;
+
+    std::cout << "Результат сложения: " << add(x, y) << '\n';
+
+#else
+    std::cout << "Неизвестный режим. Завершение работы\n";
+#endif
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
