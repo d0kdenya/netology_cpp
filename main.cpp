4e662eced76a64b40ce30d64eb30e402b25eef9e
Dan Losev <shocker.nn@mail.ru>
2025-04-17 17:19:22 +0300

    Добавил решение задачи Операции

--- main.cpp
+++ main.cpp
@@ -1,25 +1,90 @@
 #include <iostream>
+#include <numeric>
+#include <stdexcept>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+class Fraction
+{
+private:
+    long long n_;
+    long long d_;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    void normalise()
+    {
+        if (d_ == 0) throw std::invalid_argument("denominator = 0");
+        if (d_ < 0)  { n_ = -n_; d_ = -d_; }
+
+        long long g = std::gcd(n_, d_);
+        n_ /= g;  d_ /= g;
     }
 
+public:
+    Fraction(long long n = 0, long long d = 1) : n_(n), d_(d) { normalise(); }
+
+    friend Fraction operator+(Fraction a, const Fraction& b) {
+        a.n_ = a.n_*b.d_ + b.n_*a.d_;
+        a.d_ = a.d_*b.d_;
+        a.normalise();  return a;
+    }
+    friend Fraction operator-(Fraction a, const Fraction& b) {
+        a.n_ = a.n_*b.d_ - b.n_*a.d_;
+        a.d_ = a.d_*b.d_;
+        a.normalise();  return a;
+    }
+    friend Fraction operator*(Fraction a, const Fraction& b) {
+        a.n_ *= b.n_;   a.d_ *= b.d_;
+        a.normalise();  return a;
+    }
+    friend Fraction operator/(Fraction a, const Fraction& b) {
+        if (b.n_ == 0) throw std::invalid_argument("division by zero");
+        a.n_ *= b.d_;   a.d_ *= b.n_;
+        a.normalise();  return a;
+    }
+    Fraction operator-() const { return Fraction(-n_, d_); }
+
+    Fraction& operator++()  { n_ += d_;  return *this; }
+    Fraction  operator++(int) { Fraction tmp = *this; ++(*this); return tmp; }
+
+    Fraction& operator--()  { n_ -= d_;  return *this; }
+    Fraction  operator--(int) { Fraction tmp = *this; --(*this); return tmp; }
+
+    friend bool operator==(const Fraction& a,const Fraction& b)
+        { return a.n_*b.d_ == b.n_*a.d_; }
+    friend bool operator<(const Fraction& a,const Fraction& b)
+        { return a.n_*b.d_ <  b.n_*a.d_; }
+    friend bool operator!=(const Fraction& a,const Fraction& b){ return !(a==b);}
+    friend bool operator>(const Fraction& a,const Fraction& b){ return  b<a; }
+    friend bool operator<=(const Fraction& a,const Fraction& b){ return !(b<a);}
+    friend bool operator>=(const Fraction& a,const Fraction& b){ return !(a<b);}
+
+    friend std::ostream& operator<<(std::ostream& os, const Fraction& f) {
+        return os << f.n_ << '/' << f.d_;
+    }
+};
+
+int main()
+{
+    system("chcp 65001 > nul");
+
+    long long n1,d1,n2,d2;
+    std::cout << "Введите числитель дроби 1: ";
+    std::cin  >> n1;
+    std::cout << "Введите знаменатель дроби 1: ";
+    std::cin  >> d1;
+    std::cout << "Введите числитель дроби 2: ";
+    std::cin  >> n2;
+    std::cout << "Введите знаменатель дроби 2: ";
+    std::cin  >> d2;
+
+    Fraction f1(n1,d1), f2(n2,d2);
+
+    std::cout << f1 << " + "  << f2 << " = " << f1 + f2 << '\n';
+    std::cout << f1 << " - "  << f2 << " = " << f1 - f2 << '\n';
+    std::cout << f1 << " * "  << f2 << " = " << f1 * f2 << '\n';
+    std::cout << f1 << " / "  << f2 << " = " << f1 / f2 << '\n';
+
+    std::cout << "++" << f1 << " * " << f2 << " = " << (++f1 * f2) << '\n';
+    std::cout << "Значение дроби 1 = " << f1 << '\n';
+    std::cout << f1 << "-- * " << f2 << " = " << (f1-- * f2) << '\n';
+    std::cout << "Значение дроби 1 = " << f1 << '\n';
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
