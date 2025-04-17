005e9013e2ec446337263a5d869457105cb8b7bf
Dan Losev <shocker.nn@mail.ru>
2025-04-17 16:31:35 +0300

    Добавил решение задачи Сортировка Адресов

--- main.cpp
+++ main.cpp
@@ -1,25 +1,76 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <limits>
+#include <cstdlib>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+class Address {
+private:
+    std::string city, street;
+    int house, apartment;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+public:
+    Address(const std::string& c,
+            const std::string& s,
+            int h,
+            int a)
+        : city(c), street(s), house(h), apartment(a) {}
+
+    std::string toString() const {
+        return city + ", " + street + ", "
+             + std::to_string(house) + ", "
+             + std::to_string(apartment);
     }
+    const std::string& getCity() const { return city; }
+};
+
+void sort(std::vector<Address>& arr) {
+    for (std::size_t i = 0; i + 1 < arr.size(); ++i)
+        for (std::size_t j = 0; j + 1 < arr.size() - i; ++j)
+            if (arr[j].getCity() > arr[j + 1].getCity())
+                std::swap(arr[j], arr[j + 1]);
+}
+
+int main() {
+    system("chcp 65001 > nul");
+
+    std::ifstream in("../in.txt");
+    if (!in) {
+        std::cerr << "Не удалось открыть in.txt\n";
+        return 1;
+    }
+
+    int N;
+    in >> N;
+    in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::vector<Address> arr;
+    arr.reserve(N);
+
+    for (int i = 0; i < N; ++i) {
+        std::string city, street;
+        int house, apt;
+
+        std::getline(in, city);
+        std::getline(in, street);
+        in >> house >> apt;
+        in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        arr.emplace_back(city, street, house, apt);
+    }
+    in.close();
+
+    sort(arr);
+
+    std::ofstream out("../out.txt");
+    if (!out) {
+        std::cerr << "Не удалось открыть out.txt\n";
+        return 1;
+    }
+
+    out << N << '\n';
+    for (const auto& a : arr)
+        out << a.toString() << '\n';
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
