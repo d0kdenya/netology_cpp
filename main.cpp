23c28e9ab7065e3922624aaa72ca268e0f8dde78
Dan Losev <shocker.nn@mail.ru>
2025-04-17 16:58:15 +0300

    Добавил решение задачи Иерархия Классов

--- main.cpp
+++ main.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+#include "Triangle.h"
+#include "RightTriangle.h"
+#include "IsoscelesTriangle.h"
+#include "EquilateralTriangle.h"
+#include "Quadrangle.h"
+#include "Rectangle.h"
+#include "Square.h"
+#include "Parallelogram.h"
+#include "Rhombus.h"
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+static void print(const Figure* f) { f->print_info(); }
 
+int main()
+{
+    system("chcp 65001 > nul");
+
+    Triangle          tri("Треугольник", 10,20,30, 50,60,70);
+    RightTriangle     badRT(10,20,30, 50,60);
+    RightTriangle     goodRT(10,20,30, 50,40);
+    IsoscelesTriangle badIso(10,20,50,60);
+    EquilateralTriangle goodEqui(30);
+
+    Quadrangle quad("Четырёхугольник", 10,20,30,40, 50,60,70,80);
+    Rectangle  rect(10,20);
+    Square     sq(20);
+    Parallelogram badPar(20,30,30,40);
+    Rhombus    badRh(30,30,40);
+
+    std::vector<Figure*> figs = {
+        &tri, &badRT, &goodRT, &badIso, &goodEqui,
+        &quad, &rect, &sq, &badPar, &badRh
+    };
+
+    for (auto f : figs) print(f);
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
