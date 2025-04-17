95cd94df06c1f46167ffa9e1ba92792739b78fbc
Dan Losev <shocker.nn@mail.ru>
2025-04-17 16:34:57 +0300

    Добавил решение задачи Фигуры

--- main.cpp
+++ main.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Figure {
+protected:
+    int         sides_count;
+    std::string name;
+
+    Figure(const std::string& n, int sides)
+        : sides_count(sides), name(n) {}
+
+public:
+    Figure() : Figure("Фигура", 0) {}
+
+    int  get_sides_count() const { return sides_count; }
+    const std::string& get_name() const { return name; }
+};
+
+class Triangle : public Figure {
+public:
+    Triangle() : Figure("Треугольник", 3) {}
+};
+
+class Quadrangle : public Figure {
+public:
+    Quadrangle() : Figure("Четырёхугольник", 4) {}
+};
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    system("chcp 65001 > nul");
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    Figure      f;
+    Triangle    t;
+    Quadrangle  q;
 
+    std::cout << "Количество сторон:\n";
+    std::cout << f.get_name() << ": " << f.get_sides_count() << '\n';
+    std::cout << t.get_name() << ": " << t.get_sides_count() << '\n';
+    std::cout << q.get_name() << ": " << q.get_sides_count() << '\n';
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
